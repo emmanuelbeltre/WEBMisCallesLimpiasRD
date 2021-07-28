6f5bf99a3ae39ccd74dfa48d2f50678301fc73ab
emmanuelbeltre <beltrelgg2@gmail.com>
2021-07-28 15:27:03 -0400

    métodos online

--- src/app/Services/recompensas.service.ts
+++ src/app/Services/recompensas.service.ts
@@ -19,15 +19,15 @@ export class RecompensasService {
 
 
       actualizarRecompensas(cod_recompensa:any,  puntos:any,  nombre:any,  imagen:any,  descripcion:any){
-        return this.http.get('http://localhost:53059/api/MisCallesLimpiasRD/ActualizarRecompensa?cod_recompensa='+cod_recompensa+'&puntos='+puntos+'&nombre='+nombre+ '&imagen='+imagen+'&descripcion='+descripcion );
+        return this.http.get('http://api.miscalleslimpiasrd.tecnolora.com/api/MisCallesLimpiasRD/ActualizarRecompensa?cod_recompensa='+cod_recompensa+'&puntos='+puntos+'&nombre='+nombre+ '&imagen='+imagen+'&descripcion='+descripcion );
       }
 
        eliminarRecompensas(cod_recompensa: any){
-         return this.http.get('http://localhost:53059/api/MisCallesLimpiasRD/EliminarRecompensas?cod_recompensa=' + cod_recompensa);
+         return this.http.get('http://api.miscalleslimpiasrd.tecnolora.com/api/MisCallesLimpiasRD/EliminarRecompensas?cod_recompensa=' + cod_recompensa);
        }
 
        agregarRecompensas( nombre:any,puntos:any,imagen:any,descripcion:any){
-        return this.http.get('http://localhost:53059/api/MisCallesLimpiasRD/AgregarRecompensa?nombre='+nombre+'&puntos='+ puntos+'&imagen='+imagen+'&descripcion=' + descripcion );
+        return this.http.get('http://api.miscalleslimpiasrd.tecnolora.com/api/MisCallesLimpiasRD/AgregarRecompensa?nombre='+nombre+'&puntos='+ puntos+'&imagen='+imagen+'&descripcion=' + descripcion );
       }
 
 
